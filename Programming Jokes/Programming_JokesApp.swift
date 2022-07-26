e4426a818d73bf8548fc12e27effaf09d4504a7a
Kate Lee <isoeun809@gmail.com>
2022-07-26 10:50:46 -0500

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by 이소은 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
